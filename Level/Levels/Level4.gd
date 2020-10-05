3c38395181df6d3aa1c8e16c5c9703c6c4a8888a
Mai-Lan Vu <miavu11@gmail.com>
2020-10-05 22:57:21 +0100

    level supplies added

---
+++ Level/Levels/Level4.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Global.clear_items()
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Level/Levels/Level5.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Global.clear_items()
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
