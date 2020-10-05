258f2a7d4cf5fb751610ea6cc24e2f4e5df95526
Mai-Lan Vu <miavu11@gmail.com>
2020-10-05 23:11:53 +0100

    finalising lvl 6

---
+++ Level/Levels/Level6.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Global.clear_items()
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.ARROW_PLATE)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.WALLS)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.ROBO)
+	Global.deposit_item(Global.ItemType.BLOCK_TILE)
+	Global.deposit_item(Global.ItemType.BLOCK_TILE)
+	Global.deposit_item(Global.ItemType.BLOCK_TILE)
+	Global.deposit_item(Global.ItemType.BLOCK_TILE)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
